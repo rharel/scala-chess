ed980b23d34d2e22aaf947aafe66a7e183c7f9a6
Raoul Harel <admin@rharel.com>
2019-01-28 20:35:43 +0100

    Remove coordinate trait and fix square notation

--- src/main/scala/core/Square.scala
+++ src/main/scala/core/Square.scala
@@ -1,5 +1,8 @@
 package core
 
-case class Square(row: Coordinate, col: Coordinate) extends Debuggable {
-  def debug() = s"${('A' + row.index).toChar}${col.index}"
+case class Square(row: Int, col: Int) extends Debuggable {
+  assert(0 <= row && row < 8)
+  assert(0 <= col && col < 8)
+  
+  def debug() = s"${('a' + col).toChar}$row"
 }
