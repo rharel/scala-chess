f0da368277ee60e2f59c4a9557916a1ff5affd85
Raoul Harel <admin@rharel.com>
2019-01-28 20:36:39 +0100

    Add en passant move variant, rename castle variants

--- src/main/scala/core/Move.scala
+++ src/main/scala/core/Move.scala
@@ -1,12 +1,16 @@
 package core
 
 sealed trait Move extends Debuggable
-case class SimpleMove(source: Square, target: Square) extends Move {
+
+case class PieceMove(source: Square, target: Square) extends Move {
   def debug() = s"${source.debug()} => ${target.debug()}"
 }
-case object CastleKingside extends Move {
-  def debug() = "castle kingside"
+case class EnPassant(col: Int) extends Move {
+  def debug() = s"en passant at $col"
 }
-case object CastleQueenside extends Move {
-  def debug() = "castle queenside"
+case object KingsideCastle extends Move {
+  def debug() = "kingside castle"
+}
+case object QueensideCastle extends Move {
+  def debug() = "queenside castle"
 }
