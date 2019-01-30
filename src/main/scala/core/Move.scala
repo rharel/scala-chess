5bba90ecde54ae2530af345213699e4ff16f897c
Raoul Harel <admin@rharel.com>
2019-01-30 22:01:58 +0100

    Fix build errors

--- src/main/scala/core/Move.scala
+++ src/main/scala/core/Move.scala
@@ -3,7 +3,7 @@ package core
 sealed trait Move extends Debuggable
 
 case class PieceMove(source: Square, target: Square) extends Move {
-  def debug = s"${source.debug()} => ${target.debug()}"
+  def debug = s"${source.debug} => ${target.debug}"
 }
 case class EnPassant(col: Int) extends Move {
   assert(0 <= col && col < 8)
@@ -11,7 +11,7 @@ case class EnPassant(col: Int) extends Move {
 }
 case class Promote(col: Int, kind: PieceKind) extends Move {
   assert(0 <= col && col < 8)
-  def debug = s"promotion at ${('a' + col).toChar} to ${kind.debug()}"
+  def debug = s"promotion at ${('a' + col).toChar} to ${kind.debug}"
 }
 case object KingsideCastle extends Move {
   def debug = "kingside castle"
--- src/main/scala/core/Piece.scala
+++ src/main/scala/core/Piece.scala
@@ -1,5 +1,5 @@
 package core
 
 case class Piece(color: PieceColor, kind: PieceKind) extends Debuggable {
-  def debug = s"${color.debug()} ${kind.debug()}"
+  def debug = s"${color.debug} ${kind.debug}"
 }
