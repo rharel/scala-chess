16bb32a16987534cd72b82cf952e0a4e190f9fc6
Raoul Harel <admin@rharel.com>
2019-02-02 16:57:14 +0100

    Simplify square class

--- src/main/scala/core/Square.scala
+++ src/main/scala/core/Square.scala
@@ -1,11 +1,5 @@
 package core
 
-object Square {
-  def isValid(row: Row, col: Col): Boolean = row.isValid && col.isValid
-}
 final case class Square(row: Row, col: Col) {
-  assert(isValid)
-
-  def isValid: Boolean = Square.isValid(row, col)
   override def toString = s"$col$row"
 }
