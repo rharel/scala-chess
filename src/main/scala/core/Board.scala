bffbce09c0ad7e9a9a60b6d5421962f7cd6fd7ed
Raoul Harel <admin@rharel.com>
2019-02-02 16:57:50 +0100

    Implement board class

--- src/main/scala/core/Board.scala
+++ src/main/scala/core/Board.scala
@@ -1,12 +1,63 @@
 package core
 
+import CoordinateConversion.{intToRow, intToCol}
+
+object Board {
+  val SIZE = 8
+
+  def squares: Iterator[Square] = squareList.iterator
+
+  private val squareList: List[Square] = {
+    var result = List.empty[Square]
+    for (row <- 0 until Board.SIZE) {
+      for (col <- 0 until Board.SIZE) {
+        result = result :+ Square(row, col)
+      }
+    }
+    result
+  }
+}
 final class Board {
-  def at(row: Row, col: Col): Option[Piece] = throw new NotImplementedError()
-  def take(row: Row, col: Col): Option[Piece] = throw new NotImplementedError()
-  def put(row: Row, col: Col, piece: Piece) = throw new NotImplementedError()
+  def at(row: Row, col: Col): Option[Piece] = at(toGridIndex(row, col))
+  def take(row: Row, col: Col): Option[Piece] = take(toGridIndex(row, col))
+  def put(row: Row, col: Col, piece: Piece): Option[Piece] = put(toGridIndex(row, col), piece)
 
   def isFree(row: Row, col: Col): Boolean = at(row, col).isEmpty
   def isOccupied(row: Row, col: Col): Boolean = !isFree(row, col)
 
-  override def toString = throw new NotImplementedError()
+  def copy(source: Board): Unit = source.grid.copyToArray(this.grid)
+
+  override def clone: Board = {
+    val result = new Board
+    result.copy(this)
+    result
+  }
+  override def toString: String = {
+    var result = ""
+    for (Square(row, col) <- Board.squares) {
+      result += (at(row, col) match {
+        case Some(Piece(White, kind)) => kind.name(0).toUpper
+        case Some(Piece(Black, kind)) => kind.name(0).toLower
+        case None => "."
+      })
+      if (col.isLast) { result += "\n" }
+    }
+    result
+  }
+
+  private def toGridIndex(row: Row, col: Col): Int = Board.SIZE * row.index + col.index
+
+  private def at(index: Int): Option[Piece] = grid(index)
+  private def take(index: Int): Option[Piece] = {
+    val contents = grid(index)
+    grid(index) = None
+    contents
+  }
+  private def put(index: Int, piece: Piece): Option[Piece] = {
+    val previousContents = grid(index)
+    grid(index) = Some(piece)
+    previousContents
+  }
+
+  private val grid = Array.fill[Option[Piece]](Board.SIZE * Board.SIZE)(None)
 }
