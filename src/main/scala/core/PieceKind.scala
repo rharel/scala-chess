36d4fa710af9e28aacca6d0580a72cf486d8ab4b
Raoul Harel <admin@rharel.com>
2019-02-07 20:59:12 +0100

    Mark which piece kinds may be the result of pawn promotion

--- src/main/scala/core/PieceKind.scala
+++ src/main/scala/core/PieceKind.scala
@@ -4,9 +4,11 @@ sealed trait PieceKind {
   val name: String
   override def toString: String = name
 }
+sealed trait PromotionTarget
+
 case object Pawn extends PieceKind { val name = "pawn" }
-case object Knight extends PieceKind { val name = "knight" }
-case object Bishop extends PieceKind { val name = "bishop" }
-case object Rook extends PieceKind { val name = "rook" }
-case object Queen extends PieceKind { val name = "queen" }
+case object Knight extends PieceKind with PromotionTarget { val name = "knight" }
+case object Bishop extends PieceKind with PromotionTarget { val name = "bishop" }
+case object Rook extends PieceKind with PromotionTarget { val name = "rook" }
+case object Queen extends PieceKind with PromotionTarget { val name = "queen" }
 case object King extends PieceKind { val name = "king" }
