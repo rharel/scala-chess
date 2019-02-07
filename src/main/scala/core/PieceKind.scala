35a3f60e9a72b1f4f18b598526311e793cd6efa3
Raoul Harel <admin@rharel.com>
2019-02-07 21:10:17 +0100

    Better semantic typing between piece kinds and promotion targets

--- src/main/scala/core/PieceKind.scala
+++ src/main/scala/core/PieceKind.scala
@@ -4,11 +4,11 @@ sealed trait PieceKind {
   val name: String
   override def toString: String = name
 }
-sealed trait PromotionTarget
+sealed trait PromotionTarget extends PieceKind
 
 case object Pawn extends PieceKind { val name = "pawn" }
-case object Knight extends PieceKind with PromotionTarget { val name = "knight" }
-case object Bishop extends PieceKind with PromotionTarget { val name = "bishop" }
-case object Rook extends PieceKind with PromotionTarget { val name = "rook" }
-case object Queen extends PieceKind with PromotionTarget { val name = "queen" }
+case object Knight extends PromotionTarget { val name = "knight" }
+case object Bishop extends PromotionTarget { val name = "bishop" }
+case object Rook extends PromotionTarget { val name = "rook" }
+case object Queen extends PromotionTarget { val name = "queen" }
 case object King extends PieceKind { val name = "king" }
