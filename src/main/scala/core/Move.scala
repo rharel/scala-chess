34ba46ba8071395b63596eb22c952e2b901df76d
Raoul Harel <admin@rharel.com>
2019-02-09 21:48:41 +0100

    Rename PromotionTarget => PromotionPiece, expand promotion move variant to account for capture-promotion

--- src/main/scala/core/Move.scala
+++ src/main/scala/core/Move.scala
@@ -5,8 +5,8 @@ sealed trait Move
 final case class RegularMove(origin: Square, target: Square) extends Move {
   override def toString = s"$origin => $target"
 }
-final case class Promotion(col: Col, target: PromotionTarget) extends Move {
-  override def toString = s"promotion at $col to $target"
+final case class Promotion(origin: Col, target: Col, piece: PromotionPiece) extends Move {
+  override def toString = s"$origin => $target promoted to $piece"
 }
 case object KingsideCastle extends Move {
   override def toString = "kingside castle"
--- src/main/scala/core/PieceKind.scala
+++ src/main/scala/core/PieceKind.scala
@@ -4,11 +4,11 @@ sealed trait PieceKind {
   val name: String
   override def toString: String = name
 }
-sealed trait PromotionTarget extends PieceKind
+sealed trait PromotionPiece extends PieceKind
 
 case object Pawn extends PieceKind { val name = "pawn" }
-case object Knight extends PromotionTarget { val name = "knight" }
-case object Bishop extends PromotionTarget { val name = "bishop" }
-case object Rook extends PromotionTarget { val name = "rook" }
-case object Queen extends PromotionTarget { val name = "queen" }
+case object Knight extends PromotionPiece { val name = "knight" }
+case object Bishop extends PromotionPiece { val name = "bishop" }
+case object Rook extends PromotionPiece { val name = "rook" }
+case object Queen extends PromotionPiece { val name = "queen" }
 case object King extends PieceKind { val name = "king" }
