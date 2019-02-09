5d4249e242130a4fa7be67dbc85f5e934df6a974
Raoul Harel <admin@rharel.com>
2019-02-09 23:05:11 +0100

    Allow generation of all promotion kinds given origin and target columns

--- src/main/scala/core/Move.scala
+++ src/main/scala/core/Move.scala
@@ -5,7 +5,14 @@ sealed trait Move
 final case class RegularMove(origin: Square, target: Square) extends Move {
   override def toString = s"$origin => $target"
 }
+object Promotion {
+  def allFor(origin: Col, target: Col): Iterator[Promotion] = {
+    Iterator[PromotionPiece](Knight, Bishop, Rook, Queen)
+      .map(piece => Promotion(origin, target, piece))
+  }
+}
 final case class Promotion(origin: Col, target: Col, piece: PromotionPiece) extends Move {
+  assert(Math.abs(origin.index - target.index) <= 1)
   override def toString = s"$origin => $target promoted to $piece"
 }
 case object KingsideCastle extends Move {
