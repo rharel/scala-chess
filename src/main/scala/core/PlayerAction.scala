414f2b618d9a7c33eb0d66cfb43887e2bfe9c166
Raoul Harel <admin@rharel.com>
2019-02-10 17:43:40 +0100

    Avoid name conflict between player decision and game result 'resignation'

--- src/main/scala/core/PlayerAction.scala
+++ src/main/scala/core/PlayerAction.scala
@@ -2,5 +2,5 @@ package core
 
 sealed trait PlayerAction
 final case class Play(move: Move) extends PlayerAction
-case object DrawProposal extends PlayerAction
-case object Resignation extends PlayerAction
+case object ProposeDraw extends PlayerAction
+case object Resign extends PlayerAction
