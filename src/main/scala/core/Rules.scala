f21725d810d2c4d1a6b227eff4ba70e9edb3db32
Raoul Harel <admin@rharel.com>
2019-02-16 16:52:48 +0100

    Simplify Rules object methods

--- src/main/scala/core/Rules.scala
+++ src/main/scala/core/Rules.scala
@@ -1,21 +1,20 @@
 package core
 
 object Rules {
-  def getPromotionEdgeRow(player: Player): Row =
-    getPawnRow(Player.oppositeTo(player))
-  def getPromotionRow(player: Player): Row =
-    getPromotionEdgeRow(player) + getPawnMarchDirection(player)
-
-  def getPawnRow(player: Player): Row = player match {
-    case Black => Row.fromIndex(Grid.Size - 2)
-    case White => Row.fromIndex(1)
-  }
-  def getEnPassantRow(player: Player): Row = player match {
-    case Black => Row.fromIndex(3)
-    case White => Row.fromIndex(4)
-  }
   def getPawnMarchDirection(player: Player): Int = player match {
     case Black => -1
     case White => +1
   }
+  def getBaseRow(player: Player): Row = player match {
+    case Black => Row.Last
+    case White => Row.First
+  }
+  def getPawnRow(player: Player): Row =
+    getBaseRow(player) + getPawnMarchDirection(player)
+  def getEnPassantRow(player: Player): Row =
+    getBaseRow(player) + getPawnMarchDirection(player) * 4
+  def getPromotionEdgeRow(player: Player): Row =
+    getPawnRow(Player.oppositeTo(player))
+  def getPromotionRow(player: Player): Row =
+    getBaseRow(Player.oppositeTo(player))
 }
