c0077050e4ad041e36933e4604e2087beaa54b32
Raoul Harel <admin@rharel.com>
2019-02-17 00:53:39 +0100

    Rename player action ProposeDraw => OfferDraw

--- src/main/scala/core/PlayerAction.scala
+++ src/main/scala/core/PlayerAction.scala
@@ -4,8 +4,8 @@ sealed trait PlayerAction
 final case class Play(move: Move) extends PlayerAction {
   override def toString = move.toString
 }
-case object ProposeDraw extends PlayerAction {
-  override def toString = "propose draw"
+case object OfferDraw extends PlayerAction {
+  override def toString = "offer draw"
 }
 case object Resign extends PlayerAction {
   override def toString = "resign"
